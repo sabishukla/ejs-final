221e1dffdeef456d760226cfff825087ad94be2b
sabishukla <63969925+sabishukla@users.noreply.github.com>
2022-08-18 13:21:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "Hi I'm sabi Shukla and I will graduate from Chitkara University in 2023 with a degree in Computer Science. My interests are in BackEnd Engineering and I love to create beautiful performant products with delightful user experiences. on this website you can compose a blog and you can see them below here";
+
+
+const aboutContent = "This is a website where you can write your own blogs to see for other people";
+const contactContent = "TO coontact me you can email me sabishukla.cse19@chitkarauniveristy.edu.in";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", function(req, res){
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+app.listen(process.env.PORT, function() {
+  console.log("Server started on port 3000");
+});
